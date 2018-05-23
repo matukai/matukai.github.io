6146a5b73dd80b13e4c301f1f200d4fc64d56fa8
matukai <matt.tanji@gmail.com>
2018-05-22 15:25:16 -1000

    h1

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Matthew Tanji</h1>
-          <h1>Hello World</h1>
+          <h1>Welcome to my website!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
