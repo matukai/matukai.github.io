0fd31262b36c12a23274ddb74040a1836183a053
matukai <matt.tanji@gmail.com>
2018-05-22 16:06:40 -1000

    middleware

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose} from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-
-
+import rootReducer from './reducers'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const boundCompose = compose.bind(null, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ ? boundCompose(window.__REDUX_DEVTOOLS_EXTENSION__()) : boundCompose()
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
