5f0abe344eff885c2a5a5c6ce1357c6462f898ac
matukai <matt.tanji@gmail.com>
2018-05-24 13:56:14 -1000

    styles smoke tested

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+
 import Nav from './components/NavBar';
 import Main from './components/Main';
+
+import './styles.css';
 
 class App extends Component {
   render() {
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import React from 'react';
 export default () => {
   return (
     <div>
-      <h1>Home Page</h1>
+      <div className="homeImage">
+        <img src="https://media.gettyimages.com/photos/guest-attends-the-motohiro-tanji-show-during-tokyo-fashion-week-on-picture-id516227600?s=612x612" alt=""/>
+      </div>
+      <p></p>
     </div>
   )
 }
