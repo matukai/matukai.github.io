f0ed4e37c3e7df6ae0919d2a524c2d79448981f5
matukai <matt.tanji@gmail.com>
2018-05-29 15:34:12 -1000

    background image change

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,13 @@
 import React from 'react';
 
+
 export default () => {
   return (
     <div>
-      <div className="homeImage">
-        <img src="https://media.gettyimages.com/photos/guest-attends-the-motohiro-tanji-show-during-tokyo-fashion-week-on-picture-id516227600?s=612x612" alt=""/>
-      </div>
+      <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"></link>
       <div className="homeIntro">
-        <p>Hello World, my name is Matt</p>
-        <p>I like underground music and shit</p>
-        <p>Checkout this weeks new featured artist</p>
+        <h1 className="nameTitle">Matthew Tanji</h1>
       </div>
-      <div className="homeVideo">
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/ZIjSBS5GcIY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-      </div>
-      <br/><br/><br/><br/>
     </div>
   )
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,17 @@ import { Link } from 'react-router-dom';
 export default () => {
   return (
     <nav className="navBar">
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </link>
+
+    <div class="topnav" id="myTopnav">
+      <a href="/">HOME</a>
+      <a href="/about">ABOUT</a>
+      <a href="/contact">CONTACT</a>
+    </div>
+
+
     </nav>
   )
 }
